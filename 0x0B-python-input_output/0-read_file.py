0961225fb956dbf4fbddb9a5b0a5cec894cac9fe
mowabanga <itsmundia@gmail.com>
2022-10-26 00:00:34 +0300

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+
 """Python function to read contents of a text file and print them to standard output"""
+
 def read_file(filename=""):
     """read_file - reads a text file and prints to standard output
     @filename: parameter"""
-    with open("UTF8", "R") as ut:
-        rid = ut.readlines()
+    
+    with open(filename, encoding='UTF8') as ut:
+        print(ut.read(), end='')
