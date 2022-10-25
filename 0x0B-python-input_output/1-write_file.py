8f859466da87fa5f5bf4dcc41c4f8f4a61422f40
mowabanga <itsmundia@gmail.com>
2022-10-26 00:11:59 +0300

    Writing string into a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+"""Writing a string to an encoded file"""
+
+def write_file(filename="", text=""):
+    """write_file - function to write into a file
+    @filename: name of file
+    @text: string to be written into file"""
+    
+    with open(filename, encoded="UTF8") as ut:
+        return ut.write(text)
