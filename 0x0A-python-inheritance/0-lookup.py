aefdaec842e39d605f1cfd63a915d37d4083e319
mowabanga <itsmundia@gmail.com>
2022-10-26 22:39:39 +0300

    Inheritance in classes

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 """"Returns list of available attributes and methods of an object"""
 def lookup(obj):
-    """Returns list objects
-    Args:
-        obj (_type_): _description_
-    """
-    
-    
+    """returns list object"""
+    return dir(obj)
