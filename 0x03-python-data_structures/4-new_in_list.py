d5a23968cce8e9daef9abf09cd31d3f46f39d0d6
mowabanga <itsmundia@gmail.com>
2022-11-20 23:06:16 +0300

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = list(my_list)
+    length = len(my_list)
+    if idx < 0:
+        return (new_list)
+    elif idx > length:
+        return (new_list)
+    else:
+        new_list[idx] = element
+        print(new_list, '\n', my_list)
+        return [new_list, '\n', my_list]
