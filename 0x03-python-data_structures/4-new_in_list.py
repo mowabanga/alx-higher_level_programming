477d0b0d6ee038ae63bf1f8cfcb1edc9310005d6
mowabanga <itsmundia@gmail.com>
2022-11-20 23:07:49 +0300

    Replace in list copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,5 +8,4 @@ def new_in_list(my_list, idx, element):
         return (new_list)
     else:
         new_list[idx] = element
-        print(new_list, '\n', my_list)
         return [new_list, '\n', my_list]
