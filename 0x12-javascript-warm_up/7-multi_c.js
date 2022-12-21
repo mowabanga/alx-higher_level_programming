dd1d9e251d6bf9015f5e1d9c3e5d7cb7d6e5a324
mowabanga <itsmundia@gmail.com>
2022-12-21 11:22:48 +0000

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const sentence = 'C is fun';
-const mult = "";
 const number = Number.isInteger(+args[0]);
-for (let i of args) {
-  if (number === true; i < args[0]; i++) {
-    mult += sentence;
-  } else {
-    console.log('Missing number of occurences');
+if (number !== true) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < args; i++) {
+    console.log('C is fun');
   }
 }
