fd7d662b72cd0326912eef5236d650e857c1595a
mowabanga <itsmundia@gmail.com>
2023-01-09 22:47:02 +0300

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,8 +6,8 @@
 
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
-    name VARCHAR(256,
-    score INT)
+    name VARCHAR(256),
+    score INT
 );
 
 INSERT INTO second_table(id, name, score) VALUES
