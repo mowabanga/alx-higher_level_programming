662ea3311878bc6fc0a8ceeb545a5c440d69751f
mowabanga <itsmundia@gmail.com>
2023-01-09 19:40:25 +0300

    Remove database

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,5 @@
 -- Remove DB
---m Script should not fail if DB doesn't exist
+-- Script should not fail if DB doesn't exist
+-- Not allowed to  use select or show statements
+
 DROP DATABASE IF EXISTS hbtn_0c_0;
