4c99599d5577d9a6b36bf0d61ab1b4602d3ecd50
mowabanga <itsmundia@gmail.com>
2023-12-09 10:18:14 +0300

    Print list in reversed

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,7 +7,7 @@ def print_reversed_list_integer(my_list=[]):
     """
     new_list = []
     if len(my_list) < 1:
-        return"None"
+        return "None"
     for i in range(len(my_list)):
         popped = my_list.pop()
         new_list.append(popped)
