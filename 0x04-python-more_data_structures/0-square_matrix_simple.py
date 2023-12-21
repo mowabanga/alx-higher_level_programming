846e0efeec615b662a609e45ad61d47319ebfd75
mowabanga <itsmundia@gmail.com>
2023-12-21 12:24:51 +0300

    Squared matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,9 +5,6 @@ def square_matrix_simple(matrix=[]):
     Args:
         matrix (list, optional): matrix. Defaults to [].
     """
-    
     new_matrix = matrix[:]
     squared_matrix = [[x**2 for x in row] for row in matrix]
     return squared_matrix
-        
-square_matrix_simple([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
