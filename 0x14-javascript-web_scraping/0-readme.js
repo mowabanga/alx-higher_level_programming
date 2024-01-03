d5ca7d3c3b0060bc4191b8a0f155a8b64d412a3a
mowabanga <itsmundia@gmail.com>
2024-01-04 00:43:39 +0300

    First task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,14 +2,16 @@
 const fs = require('fs');
 const args = process.argv[2]
 const filePath = args
+
 fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
         if (err.code === 'ENOENT'){
-            console.error('Error: File does not exist')
+            console.error(err)
         } else {
             console.error(`Error reading the file: ${err.message}`)
         }
     } else {
         console.log(data)
     }
-})
+});
+    
