28320ca26bdcada2fa00c1b7f187a6954b28f2f0
mowabanga <itsmundia@gmail.com>
2024-01-04 01:04:14 +0300

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2]
+const file = process.argv[2];
 const string = process.argv[3];
+
 fs.writeFile(file, string, 'utf-8', (err) => {
     if (err) {
-        console.error(err)
-    } else {
-        console.log("Writing content")
+        if (err.code === 'ENOENT'){
+            console.error(err)
+        } else {
+            console.log(err.message)
+        }
     }
 });
