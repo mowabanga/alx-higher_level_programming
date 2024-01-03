92d4ff94ea0b579dd7b4baec5a6b3b447989597f
mowabanga <itsmundia@gmail.com>
2024-01-04 01:25:51 +0300

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,8 +4,8 @@ const url = process.argv[2];
 
 request(url, (error, response) => {
     if (error) {
-        console.error(error)
+        console.error(error);
     } else {
-        console.log(response.statusCode)
+        console.log(`code: ${response.statusCode}`);
     }
-})
+});
