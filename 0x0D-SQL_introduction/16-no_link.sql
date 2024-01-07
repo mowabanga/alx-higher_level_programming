84b44d1426cf072121aae4419e9bb2f8c6d995b0
mowabanga <itsmundia@gmail.com>
2024-01-07 16:04:02 +0300

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of second_table
-SELECT * FROM second_table WHERE name IS NOT NULL AND name <> '' ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name <> '' ORDER BY score DESC;
