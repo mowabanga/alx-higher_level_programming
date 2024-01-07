0b8cbb99b531d702470ed7530f5892503813ebc3
mowabanga <itsmundia@gmail.com>
2024-01-07 15:57:23 +0300

    Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score in second_table
 SELECT score, COUNT(score) AS number
 FROM second_table
-ORDER BY desc
-GROUP BY score;
+GROUP BY score
+ORDER BY number DESC;
