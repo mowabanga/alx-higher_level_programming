9e8bb7c6d70099a09b531f7e484932f0bbd8852f
mowabanga <itsmundia@gmail.com>
2024-01-21 13:59:08 +0300

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,15 @@
 -- creates user_0d_1
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Check if the user already exists
+SELECT COUNT(*) INTO @user_exists
+FROM mysql.user
+WHERE user = 'user_0d_1' AND host = '%';
+
+-- If the user does not exist, create it
+IF @user_exists = 0 THEN
+    CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
+    SELECT 'User user_0d_1 created and granted all privileges.';
+ELSE
+    SELECT 'User user_0d_1 already exists. No action taken.';
+END IF;
