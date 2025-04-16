b5c027af09463854e1656e909717cd678d36ba04
mowabanga <itsmundia@gmail.com>
2025-04-16 21:01:30 +0300

    Easter Egg

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+part_1 = str[7:10]
+
+print(part_1)
