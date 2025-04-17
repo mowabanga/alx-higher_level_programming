4b6afb96e71c98beb5dc3ff3e56f2f85970092ed
mowabanga <itsmundia@gmail.com>
2025-04-17 20:52:52 +0300

    My first toolbox

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Return the sum of a and b."""
+    return a + b
+
+def sub(a, b):
+    """Return the difference of a and b."""
+    return a - b
+
+def mul(a, b):
+    """Return the product of a and b."""
+    return a * b
+
+def div(a, b):
+    """Return the quotient of a and b."""
+    return a / b
