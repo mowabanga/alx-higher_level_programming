43cda3be0d03429bdde244536767509cae238d72
mowabanga <itsmundia@gmail.com>
2025-04-21 15:24:03 +0300

    Add main declerative

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(0, len(my_list)):
-        print("{}".format(my_list[i]))
+if __name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for i in range(0, len(my_list)):
+            print("{}".format(my_list[i]))
         
 print_list_integer([1, 2, 3])
