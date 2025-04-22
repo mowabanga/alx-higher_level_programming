c8fb35cf9e726631357259c2f39445a52d49da3b
mowabanga <itsmundia@gmail.com>
2025-04-22 03:07:12 +0300

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return ("Length: {:d} - First character: {}".format(len(sentence), sentence[:1]))
+    if (len(sentence) == 0): 
+        return None
+    else:
+        return ("Length: {:d} - First character: {}".format(len(sentence), sentence[:1]))
+    
+print(multiple_returns("At school, I learnt C!"))
