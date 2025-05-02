b5c1179c2a0f17dd85fb390811cfe258ed8b2af3
mowabanga <itsmundia@gmail.com>
2025-05-02 12:36:36 +0300

    Mul dict by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     copied_dict = a_dictionary.copy()
     for key, value in copied_dict.items():
-        value *= 2
+        copied_dict[value] = value * 2
     return (f"{key}: {value}")
